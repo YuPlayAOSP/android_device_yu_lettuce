4d00682f5c1f30465728ccc8aa5c287037ac487c
Pranav Vashi <neobuddy89@gmail.com>
2015-11-30 07:36:50 -0800

    lettuce: Disable wifiap firmware reload
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -138,3 +138,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024
+
+# Disable WiFi firmware reload
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.disableWifiApFirmwareReload=true
